b1927419ba54c4bf0de58af7cec9f0ff64b48353
an612004 <hobinhan321@gmail.com>
2025-10-20 10:57:12 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,9 @@ export default function Home() {
         <>
           <title>TN7EDU Test - Luyện thi IELTS Online</title>
           <meta name="description" content="Nền tảng luyện thi IELTS trực tuyến hàng đầu Việt Nam" />
+          <meta property="og:image" content="https://test-ielts-tn7-edu.vercel.app/_next/image?url=%2Flogo_backup.png&w=2048&q=75" />
         </>
+        
       }
     >
       <div className="w-full flex flex-col justify-center items-center">
